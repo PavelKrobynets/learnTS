7edf9bd3678d44d3e32b0cc84326c5b0e9f02fec
Pavel <krobynetspavel@gmail.com>
2024-08-27 17:13:52 +0300

    started learning ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+const str: string = "Hello world";
+
+console.log(str);
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = "Alex";
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Congrats ${userName}, age ${age + 1}`);
+}
